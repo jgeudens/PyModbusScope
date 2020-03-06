485c4c219c16451c861e6e71beb3370069682e95
Jens Geudens <jensgeudens@hotmail.com>
2020-03-06 20:09:15 +0100

    Create extra variable for main window;

--- pymodbusscope.py
+++ pymodbusscope.py
@@ -19,40 +19,41 @@ class ModbusScope:
             except ElementNotFoundError:
                 raise RuntimeError("ModbusScope is not running or in PATH")
 
-        self.app['ModbusScope.*'].set_focus()
+        self.main_window = self.app['ModbusScope.*']
+        self.main_window.set_focus()
         #self.app['ModbusScope.*'].print_control_identifiers()
 
     def load_project(self, file, path=""):
         ''' Load a project (.mbs) file from the given absolute or relative path '''
-        self.app['ModbusScope.*'].set_focus()
-        self.app['ModbusScope.*'].toolBar.LoadProjectFile.click_input()
+        self.main_window.set_focus()
+        self.main_window.toolBar.LoadProjectFile.click_input()
         if path is not "":
-            self.app['ModbusScope.*'].SelectMbsFile.BestandsNaamEdit.type_keys(path, with_spaces=True)
+            self.main_window.SelectMbsFile.BestandsNaamEdit.type_keys(path, with_spaces=True)
             send_keys("{ENTER}")
 
-        self.app['ModbusScope.*'].SelectMbsFile.BestandsNaamEdit.type_keys(file, with_spaces=True)
+        self.main_window.SelectMbsFile.BestandsNaamEdit.type_keys(file, with_spaces=True)
         send_keys("{ENTER}")
 
     def start_logging(self):
         ''' Start logging - Some registers must be added '''
-        self.app['ModbusScope.*'].set_focus()
-        self.app['ModbusScope.*'].toolBar.StartLogging.click_input()
+        self.main_window.set_focus()
+        self.main_window.toolBar.StartLogging.click_input()
 
     def stop_logging(self):
         ''' Stop the current logging '''
-        self.app['ModbusScope.*'].set_focus()
-        self.app['ModbusScope.*'].toolBar.StopLogging.click_input()
+        self.main_window.set_focus()
+        self.main_window.toolBar.StopLogging.click_input()
 
     def add_register(self, address):
         ''' Add a register at the given address '''
-        self.app['ModbusScope.*'].toolBar.RegisterSettings.click_input()
-        self.app['ModbusScope.*'].RegisterSettings.AddNewRegister.click_input()
-        self.app['ModbusScope.*'].RegisterSettings.print_control_identifiers()
+        self.main_window.toolBar.RegisterSettings.click_input()
+        self.main_window.RegisterSettings.AddNewRegister.click_input()
+        self.main_window.RegisterSettings.print_control_identifiers()
 
     def clear_data(self):
         ''' Clear the data '''
-        self.app['ModbusScope.*'].Menu2.View.print_control_identifiers()
+        self.main_window.Menu2.View.print_control_identifiers()
 
     def about(self):
         ''' Open the About menu '''
-        self.app['ModbusScope.*'].Menu2.menu_select("? -> About")
+        self.main_window.Menu2.menu_select("? -> About")
